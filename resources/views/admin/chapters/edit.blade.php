fc6b4f87a4224c0991c91fa9a7c34b07aad04e9e
chhatraman shrestha <cms111000111@gmail.com>
2022-09-08 07:42:39 +0545

    destination type managed

--- resources/views/admin/chapters/edit.blade.php
+++ resources/views/admin/chapters/edit.blade.php
@@ -5,32 +5,34 @@
 @endsection
 
 @section('s-title')
-<a href="{{ route('admin.destination.type.index') }}">Destination Types</a>
-{{$type->name}}
+    <a href="{{ route('admin.chapters.index') }}">Chapters</a> / {{$chapter->name}} / Edit
 @endsection
 
 @section('content')
-    <form action="{{ route('admin.destination.type.edit',['type'=>$type->id]) }}" enctype="multipart/form-data" method="post">
+    <form action="{{ route('admin.chapters.edit',['chapter'=>$chapter->id]) }}" enctype="multipart/form-data" method="post">
         @csrf
         <div class="bg-white shadow mb-3">
             <div class="card-body">
 
                 <div class="form-group">
                     <label for="name">Name </label>
-                    <input type="text" name="name" id="name"  class="form-control " required value="{{$type->name}}">
+                    <input type="text" name="name" id="name"  class="form-control " required value="{{$chapter->name}}">
                 </div>
                 <div class="form-group">
                     <label for="image">Image (1980 X 1000)</label>
-                    <input type="file" name="image" id="image" accept="image/*" class="form-control image"  data-default-file="{{asset($type->image)}}">
+                    <input type="file" name="image" id="image" accept="image/*" class="form-control image"  data-default-file="{{asset($chapter->image)}}">
                 </div>
-
+                <div class="form-group">
+                    <label for="short_desc">Short Description</label>
+                    <textarea name="short_desc" id="short_desc" class="form-control" required>{{$chapter->short_desc}}</textarea>
+                </div>
                 <div class="form-group">
                     <label for="desc">Full Description</label>
-                    <textarea name="desc" id="desc" class="form-control">{{$type->desc}}</textarea>
+                    <textarea name="desc" id="desc" class="form-control">{{$chapter->desc}}</textarea>
                 </div>
                 <div class="form-group">
-                    <button class="btn btn-primary">Save type</button>
-                    <a href="{{ route('admin.destination.type.index') }}" class="btn btn-danger">Cancel</a>
+                    <button class="btn btn-primary">Save Chapter</button>
+                    <a href="{{ route('admin.chapters.index') }}" class="btn btn-danger">Cancel</a>
                 </div>
             </div>
 
